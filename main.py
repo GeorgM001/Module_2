b6f96cbca51a5d354d3dc4f50f7c107358d45dcd
Georgy <grinyakraft1990@gmail.com>
2024-11-15 21:25:20 +0400

    homework module_2_3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+result = list(filter(lambda elem: elem in b, a))
+print(set(result))
