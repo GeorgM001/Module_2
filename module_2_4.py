e0b209f6e50d10bea9a68c891d971fdd933d5678
Georgy <grinyakraft1990@gmail.com>
2024-11-20 20:21:26 +0400

    homework module_2_4

--- module_2_4.py
+++ module_2_4.py
@@ -2,16 +2,16 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers:
-    if i == 1:  # 1 пропускаем
+    if i == 1:
         continue
-    is_prime = True  # все числа правда
-    for j in range(2, int(i) - 1):  # переменная j это чило от 2 до i - 1, потому что не включительно
-        if i % j == 0:  # если i делится на j и при этом остаток 0, то flag = не правда, прерывание цикла
+    is_prime = True
+    for j in range(2, int(i) - 1):
+        if i % j == 0:
             is_prime = False
             break
     if is_prime:
-        primes.append(i)  # если flag правда то число добавляется сюда
+        primes.append(i)
     else:
-        not_primes.append(i)  # если flag не правда то число добавляется сюда
+        not_primes.append(i)
 print('Primes: ', primes)
 print('Not Primes: ', not_primes)
