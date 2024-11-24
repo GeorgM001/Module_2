0952abd56314477ebe5b71739d06813a7c4ac759
Georgy <grinyakraft1990@gmail.com>
2024-11-24 17:56:26 +0400

    homework module_3_1

--- 11.py
+++ 11.py
@@ -1,6 +1,11 @@
-def main():
-    pass
+def get_mat(n,m,value):
+    mat_ = []
+    if n == 0 or m == 0:
+        return mat_
+    for row in range(n):
+        mat_.append([])
+        for col in range(m):
+            mat_[row].append(value)
+    return mat_
+print(get_mat(2,3,6))
 
-
-if __name__ == '__main__':
-    main()
