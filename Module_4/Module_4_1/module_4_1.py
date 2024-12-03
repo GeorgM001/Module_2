b9f327c28c41e884933effac452af28334895fa1
Georgy <grinyakraft1990@gmail.com>
2024-12-03 15:04:16 +0400

    Modele 4.1

---
+++ Module_4/Module_4_1/module_4_1.py
@@ -0,0 +1,14 @@
+from true_math import divide as div_t
+from fake_math import divide as div_f
+
+
+result1 = div_t(69, 3)
+result2 = div_t(3, 0)
+result3 = div_f(49, 7)
+result4 = div_f(15, 0)
+
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
