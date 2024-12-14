63210b93dad1e1664cb8f34020136690d4dcc8fe
Georgy <grinyakraft1990@gmail.com>
2024-12-14 14:38:13 +0400

    Module 5_1_2

--- Module_5/module_5_1.py
+++ Module_5/module_5_1.py
@@ -9,3 +9,21 @@ class House:
         else:
             for floor in range(1, new_floor + 1):
                 print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+
+print(h1)
+print(h2)
+
+
+print(len(h1))
+print(len(h2))
